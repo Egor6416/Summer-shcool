8977c9f742644eca01a403a7e20b4f6bc073d306
Egor6416 <alushkin.egor@yandex.ru>
2021-07-23 11:56:54 +0700

    Задача 1

--- Project1/matrix.c
+++ Project1/matrix.c
@@ -5,14 +5,14 @@
 int main()
 {
 	int a[N][N]={1,2,3,4,5,6,7,8,9};
-	for(int i=0; i<N; i++)
-	{
-		for(int j=0; j<N; j++)
+		for(int i=0; i<N; i++)
 		{
-			printf("%d ", a[i][j]);
+			for(int j=0; j<N; j++)
+			{
+				printf("%d ", a[i][j]);
+			}
+ 			printf("\n");
 		}
- 		printf("\n");
-	}
 
 	return 0;
 }
