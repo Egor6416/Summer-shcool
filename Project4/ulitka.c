ecb72630e4c760839ebc3f199b873c451ad5b337
Egor6416 <alushkin.egor@yandex.ru>
2021-07-23 12:00:32 +0700

    Задача 4

--- Project4/ulitka.c
+++ Project4/ulitka.c
@@ -4,14 +4,14 @@ int main()
 {
 int a[N][N];
 for(int i=0; i<N; i++)
- {
+  {
         for(int j; j=0; j++)
 	 {
 		a[i][j]=1;
 		printf("%d ", a[i][j]);
 		a[i][j]=a[i][j]++;
 	 }
- }
+  }
 for (int i=N; i=N; i++)
  {
 	for(int j=0; j<N; j++)
